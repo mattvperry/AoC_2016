486335a3f8a69968de148790984b5339d073cbb2
Matt Perry <Matt Perry>
2016-12-03 00:34:48 -0500

    Clean up day 3

--- day3/python/day3.py
+++ day3/python/day3.py
@@ -1,16 +1,16 @@
-from re import split
-
 def chunk(list, size):
     n = max(1, size)
     return (list[i:i + n] for i in range(0, len(list), n))
 
-def valids(triangles):
-    return sum([1 for x, y, z in triangles if x + y > z and y + z > x and x + z > y])
+def valids(sides):
+    return sum((1 for x, y, z in sides if x + y > z and y + z > x and x + z > y))
+
+def triangles(input):
+    return (map(int, t.split()) for t in input)
 
 def day3(input):
-    triangles = (map(int, split("\s*", t)) for t in input)
-    transposed = [chunk(x, 3) for x in map(list, zip(*triangles))]
-    return sum(map(valids, transposed))
+    transposed = (chunk(x, 3) for x in map(list, zip(*triangles(input))))
+    return valids(triangles(input)), sum(map(valids, transposed))
 
 input = """
   883  357  185
