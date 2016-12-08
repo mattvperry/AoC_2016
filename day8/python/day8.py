558ecf7ce0150fd981ce9bef1bc9083ba300cafb
Matt Perry <Matt Perry>
2016-12-08 00:58:48 -0500

    Day 8 python cleanup

--- day8/python/day8.py
+++ day8/python/day8.py
@@ -1,7 +1,8 @@
 from re import compile
+from functools import reduce
 
-rectRe = compile(r'(rect)( )(\d+)x(\d+)')
-rotaRe = compile(r'(rotate) (row|column) \w=(\d+) by (\d+)')
+rectRe = compile(r'(rect) (\d+)x(\d+)')
+rotaRe = compile(r'(rotate [rc])\w* \w=(\d+) by (\d+)')
 
 def makeScreen(x, y):
     return [[' '] * x for _ in range(y)]
@@ -12,37 +13,30 @@ def transpose(l):
 def parseInstruction(inst):
     rect = rectRe.search(inst)
     rota = rotaRe.search(inst)
-    return rect.groups() if rect else rota.groups()
+    a, b, c = rect.groups() if rect else rota.groups()
+    return a, int(b), int(c)
 
-def fillRect(x, y, screen):
+def fill(x, y, screen):
     return [['#'] * x + r[x:] for r in screen[:y]] + screen[y:]
 
-def rotateRow(a, b, screen):
+def rotate(a, b, screen):
     newRow = screen[a][-b:] + screen[a][:-b]
     return screen[:a] + [newRow] + screen[a + 1:]
 
-def rotateCol(a, b, screen):
-    screen = transpose(screen)
-    screen = rotateRow(a, b, screen)
-    return transpose(screen)
-
-def countLit(screen):
-    return sum(map(lambda x: x.count('#'), screen))
-
-def lightScreen(input):
+def light(input):
+    instMap = {
+        'rect':     fill,
+        'rotate r': rotate,
+        'rotate c': lambda a, b, s: transpose(rotate(a, b, transpose(s))) 
+    }
     screen = makeScreen(50, 6)
-    for cmd, place, dim1, dim2 in map(parseInstruction, input):
-        if cmd == 'rect':
-            screen = fillRect(int(dim1), int(dim2), screen)
-        if cmd == 'rotate' and place == 'row':
-            screen = rotateRow(int(dim1), int(dim2), screen)
-        if cmd == 'rotate' and place == 'column':
-            screen = rotateCol(int(dim1), int(dim2), screen)
+    for cmd, a, b in map(parseInstruction, input):
+        screen = instMap[cmd](a, b, screen)
     return screen
 
 def day8(input):
-    screen = lightScreen(input)
-    print(countLit(screen))
+    screen = light(input)
+    print(sum(r.count('#') for r in screen))
     return "\n".join("".join(x) for x in screen)
 
 input = open("../input.txt").read()
