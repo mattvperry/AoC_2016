3d50ad32de21f942f95aa6073ba35ee32ca4d346
Matt Perry <Matt Perry>
2016-12-15 01:04:31 -0500

    Haskell day15

---
+++ day15/haskell/day15.hs
@@ -0,0 +1,19 @@
+import Data.List (tails, findIndex)
+
+discs :: [(Int, Int)]
+discs = [(0, 7), (0, 13), (2, 3), (2, 5), (0, 17), (7, 19)]
+
+windows :: Int -> [a] -> [[a]]
+windows n = takeWhile ((== n) . length) . map (take n) . tails
+
+isSlot :: (Int, Int) -> Int -> Bool
+isSlot (o, p) t = (o + t) `mod` p == 0
+
+time :: [(Int, Int)] -> Maybe Int
+time ds = findIndex (all id) $ zipWith isSlot <$> repeat ds <*> runs
+    where runs = drop 1 $ flip windows [0..] (length ds)
+
+main :: IO ()
+main = do
+    print . time $ discs
+    print . time $ discs ++ [(0, 11)]
