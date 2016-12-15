3fb4838eef586e6776cfdec0b2a617de1b8d48cd
Matt Perry <Matt Perry>
2016-12-15 02:42:52 -0500

    Cleanup HS 15

--- day15/haskell/day15.hs
+++ day15/haskell/day15.hs
@@ -1,19 +1,13 @@
-import Data.List (tails, findIndex)
-
-discs :: [(Int, Int)]
-discs = [(0, 7), (0, 13), (2, 3), (2, 5), (0, 17), (7, 19)]
-
-windows :: Int -> [a] -> [[a]]
-windows n = takeWhile ((== n) . length) . map (take n) . tails
-
-isSlot :: (Int, Int) -> Int -> Bool
-isSlot (o, p) t = (o + t) `mod` p == 0
+import Control.Arrow (first)
+import Data.List (findIndex)
 
 time :: [(Int, Int)] -> Maybe Int
-time ds = findIndex (all id) $ zipWith isSlot <$> repeat ds <*> runs ds
-    where runs = drop 1 . flip windows [0..] . length
+time = findIndex (all $ uncurry isSlot) . go . zip [1..]
+    where go i = [map (first (+x)) i | x <- [0..]]
+          isSlot o (s, p) = (o + s) `mod` p == 0
 
 main :: IO ()
 main = do
+    let discs = [(0, 7), (0, 13), (2, 3), (2, 5), (0, 17), (7, 19)]
     print . time $ discs
     print . time $ discs ++ [(0, 11)]
